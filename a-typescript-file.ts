c50b6c0a32fe1e06de1b3270ad56a5fa018f4804
Andrew Jefferson <8148776+eastlondoner@users.noreply.github.com>
2022-01-01 13:09:48 +0000

    Initial commit

---
+++ a-typescript-file.ts
@@ -0,0 +1,9 @@
+type Cat = {
+  size: "Fine Boi"|"Chomnk"|"Heckin' Chonker"|"HEFTY"|"M E G A C H O N K"|"OH LAWD HE COMIN!"
+}
+
+myKitty = {
+  size: ""
+}
+
+myKitty. 
---
+++ src/a-typescript-file.ts
@@ -0,0 +1,7 @@
+type Cat = {
+  size: "Fine Boi"|"Chomnk"|"Heckin' Chonker"|"HEFTY"|"M E G A C H O N K"|"OH LAWD HE COMIN!"
+}
+
+myKitty = {
+  size: ""
+}
